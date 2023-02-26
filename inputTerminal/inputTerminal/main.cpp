23e958588bf8c8bca54376cc89bc6c392e933718
ggdominguez <ggdominguez@indra.es>
2023-02-26 18:00:37 +0100

    Class for splitting input string into a vector of pairs [notes, duration].

--- inputTerminal/inputTerminal/main.cpp
+++ inputTerminal/inputTerminal/main.cpp
@@ -2,17 +2,22 @@
 #include <stdlib.h>
 #include <iostream>
 
+#include "splitter.h"
+
 using namespace std;
 
 int main(void) {
     int line=3, t;
     string s = "";
+    Splitter split;
+    std::vector<std::pair<std::string, int>> notesSplit;
 
     while (s != ":q")
     {
         cout << "Input message: (':q' for quit): " << endl;
         getline(cin,s); // This is to input the sentence
         cout << s << "  " << endl;
+        notesSplit = split.split(s, '-');
     }
     cout << "Ending program..." << endl;
 }
