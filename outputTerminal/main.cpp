24edf4e53456e6b91d29470c39ea09700d386391
ggdominguez <ggdominguez@indra.es>
2023-02-27 17:05:38 +0100

    CanBus thread is done. Time to create dataReader Can Bus.

--- outputTerminal/main.cpp
+++ outputTerminal/main.cpp
@@ -22,29 +22,54 @@ void signalHandler(int signum) {
     mutexMode.unlock();
 }
 
+void runCanBusReceiver(bool &runningCanBus)
+{
+    while(runningCanBus){
+        std::cout << "Can Bus Running" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+    std::cout << "Exiting can bus thread " << std::endl;
+}
 
 int main()
 {
     // Registering "SIGINT" signal to the handler function "signalHandler".
     signal(SIGINT, signalHandler);
 
-    bool running = true;
-    while (running)
+    // Variable for controlling execution flow.
+    bool runningLoop = true; // global execution.
+    bool runningCanBus = false; // can bus execution.
+    bool runningCRUD = false; // CRUD execution.
+    std::thread canbusThread;
+    while (runningLoop)
     {
         // Setting time to sleep loop.
         std::chrono::milliseconds timeout(2000);
         std::this_thread::sleep_for(timeout);
+
         // Protect global variable with Mutex.
         mutexMode.lock();
         if(mode ==0){
             // Code to stop CRUD if it was active.
             // Can Bus listening.
             std::cout << "Can Bus listening" << std::endl;
-            //
+            if (!runningCanBus){
+                runningCanBus = true;
+                canbusThread = std::thread(runCanBusReceiver, std::ref(runningCanBus));
+            }
+
         }
         else if (mode ==1)
         {
             // Code to stop CAN BUS if it was active.
+            if (runningCanBus){
+                runningCanBus = false;
+                if (canbusThread.joinable())
+                {
+                    canbusThread.join();  // If canbus thread is active, wait until it stops.
+                }
+            }
+
             // CRUD.
             std::cout << "doing some CRUD" << std::endl;
         }
